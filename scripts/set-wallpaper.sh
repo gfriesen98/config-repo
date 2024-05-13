1c721d7c20294a7a919cf412a7003936d24b6b53
gfriesen98 <garettf999@gmail.com>
2024-05-13 03:15:19 -0500

    update dunstrc

--- scripts/set-wallpaper.sh
+++ scripts/set-wallpaper.sh
@@ -3,13 +3,13 @@
 papePath=$HOME/.screenlayout/wallpaper.png
 
 set_wallpaper() {
-	feh -bg-scale $papePath
+	feh --bg-scale $papePath
 	exit 0
 }
 
 set_new_wallpaper() {
 	convert $1 $papePath
-	feh -bg-scale $papePath
+	feh --bg-scale $papePath
 }
 
 while getopts ":hsi:" flag; do
